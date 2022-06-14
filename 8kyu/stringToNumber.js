ad5308803317b32ef31c8bea3f24ab166421c619
Nesha Mervin <nesha.mervin@gmail.com>
2022-06-13 21:25:18 -0400

    added solution. 13/06/22

---
+++ 8kyu/stringToNumber.js
@@ -0,0 +1,6 @@
+// Convert a String to a Number! - Instructions:
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+const stringToNumber = function(str){
+    return Number(str);
+  }
