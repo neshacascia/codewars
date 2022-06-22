7e336f575ff7a91c34a47fe268804938fcc90cdd
Nesha Mervin <nesha.mervin@gmail.com>
2022-06-21 22:42:15 -0400

    added solution. 21/06/22

---
+++ 8kyu/oppositeNumber.js
@@ -0,0 +1,4 @@
+// Opposite number
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+const opposite = (number) => number > 0 ? -Math.abs(number) : Math.abs(number);
