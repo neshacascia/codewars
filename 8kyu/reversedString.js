41feb10f358338d470b82c29e0f74e7b5c125c81
Nesha Mervin <nesha.mervin@gmail.com>
2022-06-23 22:33:21 -0400

    added solution. 23/06/22

---
+++ 8kyu/reversedString.js
@@ -0,0 +1,9 @@
+// Reversed Strings
+// Complete the solution so that it reverses the string passed into it.
+
+function solution(str){
+    let newStr = str.split('')
+    newStr.reverse()
+    return newStr.join('')
+  }
+
