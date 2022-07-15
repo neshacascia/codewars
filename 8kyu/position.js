4842f61dca7bfce4afed0adeb02fa80b640900d0
Nesha Mervin <nesha.mervin@gmail.com>
2022-07-15 13:33:45 -0400

    added solution. 15/07/22

---
+++ 8kyu/position.js
@@ -0,0 +1,10 @@
+// Find the position
+/* When provided with a letter, return its position in the alphabet.
+Input :: "a"
+Ouput :: "Position of alphabet: 1" */
+
+function position(letter){
+    let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let position = alphabet.indexOf(letter) + 1
+    return `Position of alphabet: ${position}`
+  }
