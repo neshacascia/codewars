6523976eb0a4575b34226b297883cbf274b0ee93
Nesha Mervin <nesha.mervin@gmail.com>
2022-07-16 23:36:14 -0400

    added solution. 16/07/22

---
+++ 8kyu/getVolumeOfCuboid.js
@@ -0,0 +1,4 @@
+// Volume of a Cuboid
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+const getVolumeOfCuboid = (length, width, height) => { length * width * height}
