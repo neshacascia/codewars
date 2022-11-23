2b868084feafaa6bdd998220e06437e65310d420
Nesha Mervin <nesha.mervin@gmail.com>
2022-11-23 18:51:37 -0500

    refactored solution. 23/11/22

--- 8kyu/squareSum.js
+++ 8kyu/squareSum.js
@@ -1,11 +1,11 @@
 // Square(n) Sum
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
-let sum = 0
+let sum = 0;
 
-function squareSum(numbers){
-    numbers.forEach(num => {
-        sum += Math.pow(num, 2)
-    })
-    return sum
+function squareSum(numbers) {
+  numbers.forEach(num => {
+    sum += Math.pow(num, 2);
+  });
+  return sum;
 }
