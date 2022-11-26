c8db1bcf8b45f132903558fd587e3dc2e8228996
Nesha Mervin <nesha.mervin@gmail.com>
2022-11-26 09:11:09 -0500

    added solution

---
+++ 8kyu/countBy.js
@@ -0,0 +1,13 @@
+// Count by X:
+// Create a function with two arguments that will return an array of the first n multiples of x.
+// Assume both the given number and the number of times to count will be positive numbers
+// greater than 0.
+// Return the results as an array or list ( depending on language ).
+
+function countBy(x, n) {
+  let z = [];
+  for (let i = x; z.length < n; i += x) {
+    z.push(i);
+  }
+  return z;
+}
