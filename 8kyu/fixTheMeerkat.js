9d7e94941c54a962e77dcc82d7d40045dd275356
Nesha Mervin <nesha.mervin@gmail.com>
2022-12-09 06:49:55 -0500

    added solution

---
+++ 8kyu/fixTheMeerkat.js
@@ -0,0 +1,11 @@
+// My head is at the wrong end!:
+// You're at the zoo... all the meerkats look weird. Something has gone terribly wrong
+// - someone has gone and switched their heads and tails around!
+
+// Save the animals by switching them back. You will be given an array which will have
+// three values (tail, body, head). It is your job to re-arrange the array so that the
+// animal is the right way round (head, body, tail).
+
+function fixTheMeerkat(arr) {
+  return arr.reverse();
+}
