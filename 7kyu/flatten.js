79ba4926d5f80429fd8c2f0f87d7b94a2404d42c
Nesha Mervin <nesha.mervin@gmail.com>
2022-12-31 19:33:55 -0500

    added solution

---
+++ 7kyu/flatten.js
@@ -0,0 +1,7 @@
+// Flatten:
+// Write a function that flattens an Array of Array objects into a flat Array.
+// Your function must only do one level of flattening.
+
+function flatten(array) {
+  return array.flat();
+}
