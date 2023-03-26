12f58aa0e146bfe851f5625af735d12ce5815597
Nesha Mervin <nesha.mervin@gmail.com>
2023-03-26 04:28:47 -0400

    added solution

---
+++ 7kyu/betweenExtremes.js
@@ -0,0 +1,8 @@
+// Between Extremes:
+// Given an array of numbers, return the difference between the largest and smallest values.
+// The array will contain a minimum of two elements. Input data range guarantees that max-min
+// will cause no integer overflow.
+
+function betweenExtremes(numbers) {
+  return Math.max(...numbers) - Math.min(...numbers);
+}
