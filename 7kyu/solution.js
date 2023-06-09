0eddc0858745ef765c55cc5bc69736255d17d25b
Nesha Mervin <nesha.mervin@gmail.com>
2023-06-09 17:35:23 -0400

    added solution

---
+++ 7kyu/solution.js
@@ -0,0 +1,7 @@
+// String ends with?
+// Complete the solution so that it returns true if the first argument(string)
+// passed in ends with the 2nd argument (also a string).
+
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
