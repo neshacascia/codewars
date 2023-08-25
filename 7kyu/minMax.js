f8c5e1cc4cd850ad0c94e3f8f6cf80f9e09db7da
Nesha Mervin <nesha.mervin@gmail.com>
2023-08-25 07:32:45 -0400

    added solution

---
+++ 7kyu/minMax.js
@@ -0,0 +1,12 @@
+// The highest profit wins!
+// Ben has a very simple idea to make some profit: he buys something and sells
+// it again. Of course, this wouldn't give him any profit at all if he was simply
+// to buy and sell it at the same price. Instead, he's going to buy it for the lowest
+// possible price and sell it at the highest.
+
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+function minMax(arr) {
+  let sortedArr = arr.sort((a, b) => a - b);
+  return [arr[0], arr[arr.length - 1]];
+}
