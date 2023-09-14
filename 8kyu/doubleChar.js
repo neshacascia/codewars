1d0fff29cc675836b4d9368a3213cffd43b20759
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-14 12:57:52 -0400

    added solution

---
+++ 8kyu/doubleChar.js
@@ -0,0 +1,12 @@
+// Double Char
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+function doubleChar(str) {
+  let newStr = '';
+
+  for (let i = 0; i < str.length; i++) {
+    newStr += str[i] + str[i];
+  }
+
+  return newStr;
+}
