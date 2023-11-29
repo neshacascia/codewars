5ce475ddac4d419761b03c381e65f3237d5696b9
Nesha Mervin <nesha.mervin@gmail.com>
2023-11-28 19:49:51 -0500

    added solution

---
+++ 8kyu/removeExclamationMarks.js
@@ -0,0 +1,9 @@
+// Remove exclamation marks
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+  return s
+    .split('')
+    .filter(elem => elem !== '!')
+    .join('');
+}
